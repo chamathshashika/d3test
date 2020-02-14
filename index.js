e8e59d0b8e458505ebb5939f1f2753363af6a032
Chamath <chamathshashika2012@gmail.com>
2020-02-14 22:08:42 +0530

    index

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 //course 3 - creating bar chart
 var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-var svgWidth = 500, svgHidth = 300, barPadding = 5;
+var dataset = [1, 2, 3, 4, 5];
+
+var svgWidth = 500,
+    svgHidth = 300,
+    barPadding = 5;
 var barWidth = (svgWidth / dataset.length);
 
 var svg = d3.select('svg')
-        .attr("width", svgWidth)
-        .attr("height", svgHidth);
+    .attr("width", svgWidth)
+    .attr("height", svgHidth);
+
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, svgHidth])
+
 
 var barChart = svg.selectAll("rect")
-        .data(dataset)
-        .enter()
-        .append("rect")
-        .attr("y", function (d) {
-            return svgHidth - d;
-        })
-        .attr("height", function (d) {
-            return d;
-        })
-        .attr("width", barWidth - barPadding)
-        .attr("transform", function (d, i) {
-            var translate = [barWidth * i, 0];
-            return "translate(" + translate + ")";
-        });
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function(d) {
+        return svgHidth - yScale(d);
+    })
+    .attr("height", function(d) {
+        return yScale(d);
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("transform", function(d, i) {
+        var translate = [barWidth * i, 0];
+        return "translate(" + translate + ")";
+    });
 
 
 var text = svg.selectAll("text")
-        .data(dataset)
-        .enter()
-        .append("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d) {
+        return d;
+    })
+    .attr("y", function(d, i) {
+        return svgHidth - d - 2;
+    })
+    .attr("x", function(d, i) {
+        return barWidth * i;
+    })
+    .attr("fill", "#A64C38");
 
 
 
@@ -58,4 +77,3 @@ var text = svg.selectAll("text")
 //d3.select('body').append('p').text('Third paragraph.');
 //
 //d3.selectAll('p').style('color','blue');
-
